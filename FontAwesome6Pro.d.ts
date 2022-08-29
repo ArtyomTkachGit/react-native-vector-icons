899fa1015c5450eb728f4f06a13bd2b1c0fcb5a6
Artyom Tkachenko <room5525969@gmail.com>
2022-08-29 13:08:04 +0300

    add types

---
+++ FontAwesome6Pro.d.ts
@@ -0,0 +1,6 @@
+import { Component } from "react";
+import FontAwesome5Icon, { FontAwesome6IconProps } from "./FontAwesome6";
+
+export type FontAwesome5ProIconProps = FontAwesome6IconProps;
+export { FA6Style } from "./FontAwesome6";
+export default FontAwesome5Icon;
